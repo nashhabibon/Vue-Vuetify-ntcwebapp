8aa9e554b3216b5caa26b0f7c799b98e3028363d
nashhabibon <nashrudinhabibon@gmail.com>
2022-09-26 14:11:11 +0800

    trying scss in sidebarnav

--- src/components/SideBarNav.vue
+++ src/components/SideBarNav.vue
@@ -1,78 +1,98 @@
 <template>
   <v-app style="-webkit-app-region: drag">
-    
-   <v-navigation-drawer
+     <v-navigation-drawer
       v-model="drawer"
       :mini-variant.sync="mini"
       permanent
-     color="red darken-3"
-     dark
       app
     >
-   
-    <v-list-item class="px-2 pt-1">
-      
-        <v-list-item-avatar >
-            <v-img src="@/assets/logo.svg" alt="admin" class="mx-auto " />
-        </v-list-item-avatar>
-
-       <v-list-item-content >
-            <v-list-item-title class="profilename">Nashrudin Habibon</v-list-item-title>
-            <v-list-item-subtitle class="profiletitle">Admin</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-    
-    <v-divider class="mx-3 my-2"></v-divider>
-      <v-list>
-        <div v-for="(link, i) in links" :key="link.title">
-          <v-list-item v-if="!link.subLinks" :key="i" :to="link.href" class="v-list-item">
-            <v-list-item-icon>
-              <v-icon color="#ECEFF1">{{ link.icon }}</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title class="primary--text">
-                {{
-                link.title
-                }}
-              </v-list-item-title>
-            </v-list-item-content>
+  <div class="page-wrapper chiller-theme toggled" >
+    <nav id="sidebar" class="sidebar-wrapper">
+      <div class="sidebar-content">
+          <v-list-item class="pb-2 pt-2">
+            <v-badge
+            bottom
+            color="green"
+            dot
+            offset-x="20"
+            offset-y="10"
+            >
+          <v-avatar size="33" class="mr-3">
+              <v-img src="@/assets/logo.svg" alt="admin"  />
+          </v-avatar>
+          </v-badge>
+                <div class="sidebar-header ">
+                    <div class="user-info">
+                        <span class="user-name">
+                            Nashrudin Habibon
+                        </span>
+                        <span class="user-role" >Administrator</span>
+                    </div>
+                </div>
           </v-list-item>
+          <v-divider class="mx-3 mb-2"></v-divider>
+          <!-- sidebar-header  -->
+            
+          <div class="sidebar-menu">
+            <v-list shaped>
+              <div v-for="(link, i) in links" :key="link.title">
+                <v-list-item v-if="!link.subLinks" :key="i" :to="link.href">
+                  <v-list-item-icon>
+                    <v-icon >{{ link.icon }}</v-icon>
+                  </v-list-item-icon>
 
-          <v-list-group v-else :key="link.title" no-action
-        prepend-icon="mdi-chart-line" color="#ECEFF1">
-            <template v-slot:activator>
-                <v-list-item-content>
-                  <v-list-item-title class="primary--text">
-                    {{
-                    link.title
-                    }}
-                  </v-list-item-title>
-                </v-list-item-content>
-              
-            </template>
+                  <v-list-item-content>
+                    <v-list-item-title >
+                      {{
+                      link.title
+                      }}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
 
-            <v-list-item v-for="sublink in link.subLinks" :to="sublink.href" :key="sublink.title">
-              <v-list-item-icon >
-              <v-icon color="#ECEFF1">{{ sublink.icon }}</v-icon>
-            </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title class="primary--text">
-                  {{
-                  sublink.title
-                  }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-group>
-        </div>
-      </v-list>
-    </v-navigation-drawer>
+                <v-list-group v-else :key="link.title" no-action>
+                  <template v-slot:activator>
+                        <v-list-item-icon>
+                          <v-icon>fa-duotone fa-database</v-icon>
+                        </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>
+                          {{
+                          link.title
+                          }}
+                        </v-list-item-title>
+                      </v-list-item-content>
 
+                  </template>
+
+                  <v-list-item v-for="sublink in link.subLinks" :to="sublink.href" :key="sublink.title" class="sublink">
+                    <v-list-item-icon >
+                    <v-icon color="#FFFFFF">{{ sublink.icon }}</v-icon>
+                  </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>
+                        {{
+                        sublink.title
+                        }}
+                      </v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-group>
+              </div>
+            </v-list>
+          
+          </div>
+          <!-- sidebar-menu  -->
+      </div>
+            <!-- sidebar-content  -->
+            
+    </nav>
+  </div>
+  </v-navigation-drawer>
 
     <v-app-bar
       app
-      color="#2C3A47"
+      color="#972727"
       dark
     >
       <v-app-bar-nav-icon @click.stop="mini = !mini"  class="clickable"/>
@@ -120,23 +140,23 @@
         icon: "mdi-home-outline"
       },
       {
-        icon: "mdi-file-chart",
-        title: "Reports",
+        
+        title: "Database",
         subLinks: [
           {
-            icon: "mdi-file-chart",
-            title: "Sales",
-            href: "/sales"
+            icon: "fa-duotone fa-server",
+            title: "Permits",
+            href: "purchase"
           },
           {
             icon: "mdi-network-pos",
-            title: "Orders",
+            title: "Station Licenses",
             href: "/orders"
           },
           {
-            icon: "fa-solid fa-cart-flatbed-empty",
-            title: "Inventory",
-            href: "/inventory"
+            icon: "mdi-certificate-outline",
+            title: "Certificate",
+            href: "/certificates"
           }
         ]
       },
@@ -149,7 +169,8 @@
     }),
   }
 </script>
-<style lang="scss">
-// @import '@/scss/sidebarnav';
+<style lang="scss" scoped>
+@import '@/scss/sidebarnav';
 
-</style>
+</style>>
+
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,4 +8,5 @@ export default new Vuetify({
     icons: {
         iconfont: 'fa',
       },
+    theme: {dark: true}
 });
